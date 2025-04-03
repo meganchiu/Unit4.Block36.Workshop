d56254537320e585dcc48b8ba671129e4de49813
Megan Chiu <megan.chiu.2014@gmail.com>
2025-04-03 03:15:50 -0700

    setup .env file correctly

--- server/db.js
+++ server/db.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_store_db');
 const uuid = require('uuid');
